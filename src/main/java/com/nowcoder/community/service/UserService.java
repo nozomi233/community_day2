bd5294c37ec4f8426d9f9116dfa3e0914bf2fc86
zhu_nozomi <2283288415@qq.com>
2021-02-08 19:34:37 +0800

    3.27发送列表

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -165,4 +165,7 @@ public class UserService implements CommunityConstant {
         return userMapper.updateHeader(userId, headerUrl);
     }
 
+    public User findUserByName(String username){
+        return userMapper.selectByName(username);
+    }
 }
