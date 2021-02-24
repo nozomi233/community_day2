cbc4bf0460fdcbe0e0ff39ef6103e0ba2eaa6820
zhu_nozomi <2283288415@qq.com>
2021-02-24 17:17:41 +0800

    7.13任务执行和调度

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -6,8 +6,12 @@ import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.util.CommunityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
@@ -25,6 +29,8 @@ import java.util.Date;
 //@Scope("prototype")
 public class AlphaService {
 
+    private static final Logger logger = LoggerFactory.getLogger(AlphaService.class);
+
     @Autowired
     private AlphaDao alphaDao;
 
@@ -32,7 +38,7 @@ public class AlphaService {
     private UserMapper userMapper;
 
     @Autowired
-    DiscussPostMapper discussPostMapper;
+    private DiscussPostMapper discussPostMapper;
 
     @Autowired
     private TransactionTemplate transactionTemplate;
@@ -55,15 +61,15 @@ public class AlphaService {
         return alphaDao.select();
     }
 
-    // REQUIRED: 支持当前事务(外部事物)，如果不存在则创建新事务。
-    // REQUIRES_NEW: 创建一个新事物，并且暂停当前事务(外部事物)。
-    // NESTED: 如果当前存在事务(外部事物)，则嵌套在该事物中执行(独立的提交和回滚)，否则同REQUIRED。
+    // REQUIRED: 支持当前事务(外部事务),如果不存在则创建新事务.
+    // REQUIRES_NEW: 创建一个新事务,并且暂停当前事务(外部事务).
+    // NESTED: 如果当前存在事务(外部事务),则嵌套在该事务中执行(独立的提交和回滚),否则就会REQUIRED一样.
     @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
-    public Object save1(){
+    public Object save1() {
         // 新增用户
         User user = new User();
         user.setUsername("alpha");
-        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
         user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
         user.setEmail("alpha@qq.com");
         user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
@@ -74,7 +80,7 @@ public class AlphaService {
         DiscussPost post = new DiscussPost();
         post.setUserId(user.getId());
         post.setTitle("Hello");
-        post.setContent("新人报道！");
+        post.setContent("新人报道!");
         post.setCreateTime(new Date());
         discussPostMapper.insertDiscussPost(post);
 
@@ -83,17 +89,17 @@ public class AlphaService {
         return "ok";
     }
 
-    public Object save2(){
+    public Object save2() {
         transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
         transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
 
         return transactionTemplate.execute(new TransactionCallback<Object>() {
             @Override
-            public Object doInTransaction(TransactionStatus transactionStatus) {
+            public Object doInTransaction(TransactionStatus status) {
                 // 新增用户
                 User user = new User();
                 user.setUsername("beta");
-                user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+                user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
                 user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
                 user.setEmail("beta@qq.com");
                 user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
@@ -104,7 +110,7 @@ public class AlphaService {
                 DiscussPost post = new DiscussPost();
                 post.setUserId(user.getId());
                 post.setTitle("你好");
-                post.setContent("我是新人！");
+                post.setContent("我是新人!");
                 post.setCreateTime(new Date());
                 discussPostMapper.insertDiscussPost(post);
 
@@ -113,6 +119,17 @@ public class AlphaService {
                 return "ok";
             }
         });
+    }
+
+    // 让该方法在多线程环境下,被异步的调用.
+    @Async
+    public void execute1() {
+        logger.debug("execute1");
+    }
+
+    @Scheduled(initialDelay = 10000, fixedRate = 1000)
+    public void execute2() {
+        logger.debug("execute2");
     }
 
 }
